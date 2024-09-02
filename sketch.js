475568667264b983d45f8c158cfef8acf5f744db
MICAELA_SOCHA <ms22bm@fsu.edu>
2024-09-02 14:19:20 -0400

    My Field!

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(125);
+  background(135, 206, 235);
+
+  fill("yellow")
+  stroke("orange")
+  strokeWeight(20)
+  circle(550, 50, 100);
+  
+  stroke(0)
+  strokeWeight(1)
+  fill("green")
+  rect(0, 200, 600, 200)
+  
+  textSize(75)
+  text("🐸", 100, 250);
+  text("🦋", mouseX, mouseY);
 }
